ee7b96a124c7adec2d38c6ab861600ca959f280f
Veronica <veromartina8@gmail.com>
2024-11-12 20:50:15 -0300

    input ,ocultar contraseña,mostrar lista por ingreso en input,eliminar toda la lista

--- ejercicios/src/App.jsx
+++ ejercicios/src/App.jsx
@@ -1,41 +1,21 @@
-import Layout from "./components/Layout";
-import Button from "./components/Button";
-import "./components/EstilosComp.css"; // Importa los estilos
+import './App.css'
+import { TaskList } from './components/TaskList'
+import { Ejercicio } from './components/Ejercicio'
+import { FormControlado } from './components/FormControlado'
+import { FormNoControlado } from './components/FormNoControlado'
 
-const App = () => {
+
+function App() {
+
   return (
-    <Layout>
-      <div className="card-container">
-        <div className="card">
-          <h2>Card 1</h2>
-          <p>Este es el contenido de la primera card.</p>
-          <div className="conteBoton">
-            <Button style={{ backgroundColor: "blue", color: "white" }}>
-              Botón Azul
-            </Button>
-          </div>
-        </div>
-        <div className="card">
-          <h2>Card 2</h2>
-          <p>Este es el contenido de la segunda card.</p>
-          <div className="conteBoton">
-          <Button style={{ backgroundColor: "green", color: "white" }}>
-            Botón Verde
-          </Button>
-          </div>
-        </div>
-        <div className="card">
-          <h2>Card 3</h2>
-          <p>Este es el contenido de la tercera card.</p>
-          <div className="conteBoton">
-          <Button style={{ backgroundColor: "red", color: "white" }}>
-            Botón Rojo
-          </Button>
-          </div>
-        </div>
+      <div>
+         <FormNoControlado /> 
+         <FormControlado /> 
+         <Ejercicio /> 
+    <TaskList />
       </div>
-    </Layout>
-  );
-};
+     
+  )
+}
 
-export default App;
+export default App
